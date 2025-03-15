0f7bacb502bdafd6e6ad867391256fdb0a22320a
Deelz <deelaka2004@gmail.com>
2025-03-15 12:13:26 +1100

    Created structure for the add_pin file

---
+++ pins.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {*} location 
+ * @param {*} song 
+ * @param {*} date 
+ * @param {*} time 
+ */
+function add_pin(location,song,date,time) {
+    /** Create a new instance of a pin */
+
+    /** Adds pin to the database */
+}
+
