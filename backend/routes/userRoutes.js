6df4f607e2ed88b46ade9540ce2d176bac366b53
gg-kr <195712362+gg-kr@users.noreply.github.com>
2025-03-15 16:50:56 +1100

    add gitignore and add route to get all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -15,4 +15,14 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 module.exports = router;
