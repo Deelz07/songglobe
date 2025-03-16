762130d625aadfa7f46e6661152765cdc826339f
wtan0144 <wtan0144@student.monash.edu>
2025-03-16 12:48:46 +1100

    added unversioned files including package.json

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from './Map'; // Import the Map component
+import './App.css'; // Import your CSS (adjust if needed)
+
+function App() {
+    return (
+        <div className="App">
+            <Map />
+        </div>
+    );
+}
+
+export default App;
